c1f13e9c14e46a69187a4d0fbf400a8549d12617
krtffl <didac.pereze@gmail.com>
2023-06-08 15:51:00 +0200

    chore: init

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+function sayMyName(name: string): void {
+    if (name === 'Heisenberg') {
+        console.log("you're right");
+    } else {
+        console.log("you're not right");
+    }
+}
+
+sayMyName('pedro');
