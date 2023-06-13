294af10891c2ac202ed888acd0994aac42fb417a
krtffl <didac.pereze@gmail.com>
2023-06-13 23:13:10 +0200

    feat: get session radios and filter by driver

--- src/ergast.ts
+++ src/ergast.ts
@@ -6,35 +6,76 @@ export const ERGAST_API = 'https://ergast.com/api/f1';
 type ErgastResponse = {
     MRData: {
         RaceTable: {
-            Races: ErgastRace[];
+            Races: ErgastGrandPrix[];
         };
     };
 };
 
-export type ErgastRace = {
+export type ErgastGrandPrix = {
+    round: string;
+    season: string;
+    date: string;
     raceName: string;
-    date: Date;
+    FirstPractice: ErgastSession;
+    SecondPractice: ErgastSession;
+    ThirdPractice?: ErgastSession;
+    Qualifying: ErgastSession;
+    Sprint?: ErgastSession;
 };
 
-const parseResponse = (data: ErgastResponse): ErgastRace => {
+type ErgastSession = {
+    date: string;
+    time: string;
+};
+
+const parseResponse = (data: ErgastResponse): ErgastGrandPrix[] => {
     const races = data.MRData.RaceTable.Races ?? [];
-    if (_.isEmpty(races) || _.isEmpty(races[0])) {
+    if (_.isEmpty(races)) {
         throw new Error(`could not find any race information`);
     }
-    if (races.length > 1) {
-        throw new Error(`received information for more than one race`);
-    }
-    return { raceName: races[0].raceName, date: new Date(races[0].date) };
+    return races.map((race) => ({
+        round: race.round,
+        season: race.season,
+        raceName: race.raceName,
+        date: race.date,
+        FirstPractice: race.FirstPractice,
+        SecondPractice: race.SecondPractice,
+        ThirdPractice: race.ThirdPractice,
+        Qualifying: race.Qualifying,
+        Sprint: race.Sprint,
+    }));
 };
 
-//TODO allow searching by race or circuit name too
-export const getRace = async (year: number, round: number) => {
+export const getGrandPrix = async (year: number, round: number | string): Promise<ErgastGrandPrix> => {
     try {
         console.log(`fetching race ${round} from ${year}`);
-        const { data } = await axios.get<ErgastResponse>(`${ERGAST_API}/${year}/${round}.json`);
-        return parseResponse(data);
+        if (typeof round === 'number') {
+            const { data } = await axios.get<ErgastResponse>(`${ERGAST_API}/${year}/${round}.json`);
+            const races = parseResponse(data);
+            if (!races || !races[0]) {
+                throw new Error(`could not get race ${round} from ${year}`);
+            }
+            return races[0];
+        }
+        const season = await getSeason(year);
+        const race = season.find((race) => race.raceName.includes(round)); // TODO add fuzzy match
+        if (!race) {
+            throw new Error(`could not get race ${round} from ${year}`);
+        }
+        return race;
     } catch (e) {
         console.error(`error getting race ${round} from ${year}. ${e}`);
+        throw e;
+    }
+};
+
+export const getSeason = async (year: number): Promise<ErgastGrandPrix[]> => {
+    try {
+        console.log(`fetching races from ${year}`);
+        const { data } = await axios.get<ErgastResponse>(`${ERGAST_API}/${year}.json`);
+        return parseResponse(data);
+    } catch (e) {
+        console.error(`error getting races from ${year}. ${e}`);
         throw e;
     }
 };
--- src/liveTiming.ts
+++ src/liveTiming.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
-import dayjs from 'dayjs';
-
-import { ErgastRace } from './ergast';
+import dayjs, { Dayjs } from 'dayjs';
+import { ErgastGrandPrix } from './ergast';
 
 export const LIVETIMING_API = 'https://livetiming.formula1.com/static';
 
@@ -10,6 +9,8 @@ const TEAM_RADIO = new RegExp(/TeamRadio\/[A-Z]{6}[0-9]{2}_[0-9]{1,2}_[0-9]{8}_[
 export enum Session {
     Race = 'Race',
     Quali = 'Qualifying',
+    Sprint = 'Sprint',
+    Shootout = 'Sprint_Shootout',
     FP1 = 'Practice_1',
     FP2 = 'Practice_2',
     FP3 = 'Practice_3',
@@ -17,51 +18,91 @@ export enum Session {
 
 type TeamRadio = {
     path: string;
-    // driverNumber: number;
-    //timestamp: Date;
+    driverNumber?: string;
+    driver?: string;
+    timestamp?: string;
 };
 
-const parseRequest = (race: ErgastRace, session: Session): string => {
-    try {
-        const date = dayjs(race.date).format('YYYY-MM-DD');
-        return LIVETIMING_API.concat(
-            '/',
-            race.date.getFullYear().toString(),
-            '/',
-            date,
-            '_',
-            race.raceName.split(' ').join('_'),
-            '/',
-            date,
-            '_',
-            session,
-            '/',
-            'TeamRadio.jsonStream',
-        );
-    } catch (e) {
-        throw new Error(`could not parse team radios for ${race.raceName} ${session} from ${race.date}. ${e}`);
-    }
-};
-
-const parseResponse = (data: string): TeamRadio[] => {
+const parseResponse = (data: string, url: string): TeamRadio[] => {
     const radios = data.match(TEAM_RADIO);
     if (!radios) {
         throw new Error(`could not find team radios`);
     }
-    //TODO map driver number and timestamp
     return radios.map((r) => ({
-        path: r,
+        path: url.replace('TeamRadio.jsonStream', r),
+        driver: r.split('/')[1]?.split('_')[0]?.slice(0, 6),
+        driverNumber: r.split('_')[1],
+        timestamp: r.split('_')[3]?.split('.')[0],
     }));
 };
 
-export const getTeamRadios = async (race: ErgastRace, session: Session): Promise<TeamRadio[]> => {
+export const getSessionRadios = async (race: ErgastGrandPrix, session: Session): Promise<TeamRadio[]> => {
+    console.log(`fetching radios from ${session} ${race.raceName}`);
+    const url = getSessionRadiosURL(race, session)?.href;
+    if (!url) {
+        throw new Error(`could not find ${race.raceName} ${session}`);
+    }
     try {
-        const url = parseRequest(race, session);
-        console.log('URL', url);
         const { data } = await axios.get<string>(url);
-        return parseResponse(data);
+        return parseResponse(data, url);
     } catch (e) {
-        console.error(`error getting ${session} team radios for ${JSON.stringify(race)}. ${e}`);
+        console.error(`error getting team radios for ${race.raceName}'s ${session}`);
         throw e;
     }
+};
+
+export const getDriverSessionRadios = async (
+    driver: string,
+    race: ErgastGrandPrix,
+    session: Session,
+): Promise<TeamRadio[]> => {
+    const radios = await getSessionRadios(race, session);
+    return radios.filter((r) => r.driver === driver);
+};
+
+const getSessionDate = (race: ErgastGrandPrix, session: Session): Dayjs | undefined => {
+    let date;
+    switch (session) {
+        case Session.Race:
+            date = dayjs(race.date);
+            break;
+        case Session.Quali:
+            date = dayjs(race.Qualifying.date);
+            break;
+        case Session.Sprint:
+            date = dayjs(race.Sprint?.date);
+            break;
+        case Session.FP1:
+            date = dayjs(race.FirstPractice.date);
+            break;
+        case Session.FP2:
+            date = dayjs(race.SecondPractice.date);
+            break;
+        case Session.FP3:
+            date = dayjs(race.ThirdPractice?.date);
+            break;
+    }
+    return date;
+};
+
+const getSessionRadiosURL = (race: ErgastGrandPrix, session: Session): URL | undefined => {
+    const sessionDate = getSessionDate(race, session);
+    const raceDate = getSessionDate(race, Session.Race);
+    if (!sessionDate || !raceDate) return undefined;
+    return new URL(
+        LIVETIMING_API.concat(
+            '/',
+            raceDate.year().toString(),
+            '/',
+            raceDate.format('YYYY-MM-DD'),
+            '_',
+            race.raceName.split(' ').join('_'),
+            '/',
+            sessionDate.format('YYYY-MM-DD'),
+            '_',
+            session,
+            '/',
+            'TeamRadio.jsonStream',
+        ),
+    );
 };
